f4a226304a5015a55c5159c5eb055ee9e42020ca
Yeonho Lee <yhl080519@gmail.com>
2025-12-25 00:58:54 +0900

    Separate the step count into a variable

--- core.py
+++ core.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+STEP_COUNT = 5000
+
 def calculate(l_val, c_val, r_val, v_val):
     # l_val : inductance
     # c_val : capacitance
     # r_val : resistance
     # v_val : maximum voltage
-    x = np.linspace(0, np.pi*6, 5000)
+    x = np.linspace(0, np.pi*6, STEP_COUNT)
     v = v_val * np.cos(x)
     return x, v, None, None
